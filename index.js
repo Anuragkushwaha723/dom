c6df9eabfcec11eb7c82f17c287306986945e57d
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2022-11-21 14:56:39 +0530

    FourthCommit

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-// let list=document.querySelector('.list-group-item:nth-child(2)');
-// let list1=document.querySelector('.list-group-item:nth-child(3)');
-// list.style.backgroundColor='green';
-// list1.style.visibility='hidden';
+var item=document.querySelector('#items');
+// console.log(item.parentElement);
+// item.parentElement.style.backgroundColor='grey';
 
-var itemsList=document.querySelectorAll('li');
-console.log(itemsList[1].innerHTML);
-itemsList[1].style.color='green';
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for (let i = 0; i <odd.length; i++) {
-    odd[i].style.backgroundColor='green';
-    
-}
+// console.log(item.lastElementChild);
+// item.lastElementChild.textContent="hello 4321";
+
+// console.log(item.lastChild);
+
+// console.log(item.firstElementChild.textContent='Hello 1');
+
+// console.log(item.firstChild);
+
+// console.log(item.nextSibling);
+
+// console.log(item.nextElementSibling);
+
+// console.log(item.previousSibling);
+// console.log(item.previousElementSibling);
+
+var div=document.createElement('div');
+div.className='mine';
+div.id='mera';
+div.setAttribute('title','I am there for u');
+
+var divText=document.createTextNode("Hello");
+div.appendChild(divText);
+console.log(div);
+div.style.fontSize='20px';
+// var head=document.querySelector('header h1');
+// var container=document.querySelector('header .container');
+// container.insertBefore(div,head);
+var ul=document.querySelector('ul');
+var li=document.querySelector('li');
+ul.insertBefore(div,li);
